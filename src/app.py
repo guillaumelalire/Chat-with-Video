1a0b308badd8ba2d31da0e894b4bf5906951aa69
Guillaume Lalire <guillaume.lalire@epita.fr>
2024-09-06 18:19:23 +0200

    feat: added compatibility with local videos

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ import time
 from rag import create_vector_storage, launch_model, llm_response
 from audio import extract_audio, transcript, translate_to_english
 
+VIDEO_FILEPATH = "video.mp4"
 AUDIO_FILEPATH = "audio.mp3"
 
 if 'stage' not in st.session_state:
@@ -12,29 +13,40 @@ if 'stage' not in st.session_state:
 def change_stage(stage):
     st.session_state.stage = stage
 
-st.title("Chat with Video")
+st.title("Chat with Video:clapper:")
 
 if st.session_state.stage == 0:
     form = st.form("my_form")
-    uploaded_file = form.file_uploader("Choose a file", accept_multiple_files=False)
+    uploaded_file = form.file_uploader("Choose a local file", accept_multiple_files=False)
     youtube_link = form.text_input(
         "or provide the link to a YouTube video",
         "",
     )
     submit = form.form_submit_button()
-    if submit:
-        st.session_state.uploaded_file = uploaded_file
+    if (uploaded_file or youtube_link) and submit:
+        if uploaded_file and uploaded_file:
+            with open(VIDEO_FILEPATH, 'wb') as f:
+                f.write(uploaded_file.getvalue())
+            st.session_state.uploaded_file = VIDEO_FILEPATH
+        else:
+            st.session_state.uploaded_file = ""
         st.session_state.youtube_link = youtube_link
         change_stage(1)
 
 if st.session_state.stage == 1:
     with st.status("Processing video...", expanded=True) as status:
-        st.write("Extracting audio...")
-        st.session_state.video_name = extract_audio(st.session_state.uploaded_file, st.session_state.youtube_link, AUDIO_FILEPATH)
-        
-        st.write("Transcribing...")
-        transcription, language = transcript(AUDIO_FILEPATH)
-        
+        try:
+            st.write("Extracting audio...")
+            st.session_state.video_name = extract_audio(st.session_state.uploaded_file, st.session_state.youtube_link, AUDIO_FILEPATH)
+            if uploaded_file:
+                st.session_state.video_name = uploaded_file.name
+    
+            st.write("Transcribing...")
+            transcription, language = transcript(AUDIO_FILEPATH)
+        except:
+            change_stage(0)
+            raise ValueError("Invalid file or YouTube link.")
+
         if language != "en":
             st.write("Translating transcription...")
             transcription = translate_to_english(transcription, language)
@@ -52,7 +64,6 @@ if st.session_state.stage == 1:
 
 if st.session_state.stage == 3:
     st.status(label=f"Processing of **{st.session_state.video_name}** complete!", state="complete", expanded=False)
-    st.write(transcription)
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -68,11 +79,11 @@ def generate_response(prompt):
     response = llm_response(st.session_state.rag_chain, prompt)
     for word in response.split(" "):
         yield word + " "
-        time.sleep(0.01)
-        
+        time.sleep(0.05)
+
 # Accept user input
 if st.session_state.stage > 0:  
-    change_stage(4)
+    change_stage(3)
     prompt = st.chat_input("What is up?")
     if prompt:
     # Display user message in chat message container
